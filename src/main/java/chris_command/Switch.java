4b907e05a473219317d983389d1088c85ccfa159
Utilisateur <user@debian.arcahe.ovh>
2020-03-11 12:33:28 +0100

    ajout d'autres classes

---
+++ src/main/java/chris_command/Switch.java
@@ -0,0 +1,24 @@
+package chris_command;
+
+public class Switch {
+	private boolean switchOn;
+
+	/**
+	 * @param switchOn
+	 */
+	public Switch() {
+		this.switchOn = true;
+	}
+	
+	public boolean isOn() {
+		return switchOn;
+	}
+	
+	public void turnOff() {
+		switchOn = false;
+	}	
+	
+	public void turnOn() {
+		switchOn = true;
+	}	
+}
