857d23e5371922c72d78d0fe2a0b247c8847d718
cricri3007 <christophe.varengo@ens.uvsq.fr>
2020-03-29 18:34:54 +0200

    Create ErreurSaisieException.java

---
+++ src/main/java/chris_command/exc/ErreurSaisieException.java
@@ -0,0 +1,12 @@
+packagechris_command.exc;
+
+public class ErreurSaisieException extends IllegalArgumentException {
+
+	/**
+	 * Constructeur de l'exception
+	 */
+	public ErreurSaisieException() {
+		super("Erreur de saisie.");
+	}
+
+}
