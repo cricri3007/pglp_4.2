46cb4520cfb3d35267bac64c0557531335cd3764
cricri3007 <christophe.varengo@ens.uvsq.fr>
2020-03-29 12:39:32 +0200

    Create QuitCommand.java

---
+++ src/main/java/chris_command/cmd/QuitCommand.java
@@ -0,0 +1,18 @@
+package chris_command.cmd;
+import fr.uvsq.pglp.command.rpn.Switch;
+
+public class QuitCommand implements Command {
+	
+	/** # RECEIVER: Switch **/
+	
+	private Switch interrupteur;
+	
+	public QuitCommand(Switch interrupteur) {
+		this.interrupteur = interrupteur;
+	}
+
+	@Override
+	public void apply() {
+		interrupteur.turnOff();
+	}
+}
